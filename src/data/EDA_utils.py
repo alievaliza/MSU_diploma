1c77df2ba763fdf336116b56916e33cbdb7d85de
alievaliza <alieva_liza@mail.ru>
2021-10-13 15:46:25 +0300

    добавила выводы и некоторые таблицы, графики

--- src/data/EDA_utils.py
+++ src/data/EDA_utils.py
@@ -1,9 +1,13 @@
+import matplotlib
 from matplotlib import pyplot
 from typing import Dict
 import pandas as pd
 import numpy as np
 from IPython.display import display
 import seaborn as sns
+
+matplotlib.rcParams['figure.figsize'] = (10, 10)
+sns.set_style('whitegrid')
 
 def fill_prof_area(prof_areas: Dict, row: str):
   """
@@ -17,18 +21,29 @@ def fill_prof_area(prof_areas: Dict, row: str):
     else:
       prof_areas[el] = 1
 
-def find_best_prof_area(prof_areas: Dict, row: str) -> str:
+def find_best_prof_area(prof_areas: Dict, row: str, type: str) -> str:
   """
   Создаём метод, который из всех специализаций выбирает ту, которая встречается чаще всего
   Аргумент:
   row - строка в датафрейме
   """
-  max = 1
-  for el in row:
-    if prof_areas[el] > max:
-      res = el
-      max = prof_areas[el]
-  return el
+  if type == 'max':
+    max = 1
+    for el in row:
+      if prof_areas[el] > max:
+        res = el
+        max = prof_areas[el]
+  elif type == 'min':
+    min = 10**10
+    for el in row:
+      if prof_areas[el] < min:
+        res = el
+        min = prof_areas[el]
+  elif type == 'last':
+    res = row[-1]
+  else:
+    res = row[0]
+  return res
 
 def plot_dict(d: Dict):
   """
@@ -41,28 +56,24 @@ def plot_dict(d: Dict):
   sns.barplot(x=keys, y=vals, color='green')
   pyplot.xticks(rotation=45, ha="right");
 
-def plot_salary(df: pd.DataFrame, col: str, Q: float, currency: bool):
+def plot_salary(df: pd.DataFrame, col: str, currency: bool):
   """
   Создаём метод, который рисует распределение зарплат по какому-то параметру
   Аргументы:
   df - датафрейм со всем массивом данных
   col - по какому параметру (столбцу) строим распределение
-  Q - выше какого-то перцентиля не учитываем з/п
   currency - бинарная переменная, нужно ли условие на российскую валюту
   """
   for el in df[col].dropna().unique():
-    Q_from = np.nanpercentile(df['salary_from'][df[col] == el], Q)
-    Q_to = np.nanpercentile(df['salary_to'][df[col] == el], Q)
-    Q_avg = np.nanpercentile(df['salary_avg'][df[col] == el], Q)
     if currency:
-      sns.kdeplot(df['salary_avg'][df['salary_avg'] < Q_avg][df[col] == el][df['salary_currency'] == 'RUR']/1000, color = 'green', label='среднне')
-      sns.kdeplot(df['salary_from'][df['salary_from'] < Q_from][df[col] == el][df['salary_currency'] == 'RUR']/1000, color = 'blue', label='от')
-      sns.kdeplot(df['salary_to'][df['salary_to'] < Q_to][df[col] == el][df['salary_currency'] == 'RUR']/1000, color = 'orange', label='до')
-      pyplot.xlim(0, 200)
+      sns.kdeplot(df['salary_avg'][df[col] == el][df['salary_currency'] == 'RUR']/1000, color = 'green', label='среднне')
+      sns.kdeplot(df['salary_from'][df[col] == el][df['salary_currency'] == 'RUR']/1000, color = 'blue', label='от')
+      sns.kdeplot(df['salary_to'][df[col] == el][df['salary_currency'] == 'RUR']/1000, color = 'orange', label='до')
+      pyplot.xlim(10, 230)
     else:
-      sns.kdeplot(df['salary_avg'][df['salary_avg'] < Q_avg][df[col] == el]/1000, color = 'green', label='среднне')
-      sns.kdeplot(df['salary_from'][df['salary_from'] < Q_from][df[col] == el]/1000, color = 'blue', label='от')
-      sns.kdeplot(df['salary_to'][df['salary_to'] < Q_to][df[col] == el]/1000, color = 'orange', label='до')
+      sns.kdeplot(df['salary_avg'][df[col] == el]/1000, color = 'green', label='среднне')
+      sns.kdeplot(df['salary_from'][df[col] == el]/1000, color = 'blue', label='от')
+      sns.kdeplot(df['salary_to'][df[col] == el]/1000, color = 'orange', label='до')
     pyplot.legend(loc='upper right')
     pyplot.title(el)
     pyplot.xlabel("З/п, тыс.")
@@ -86,3 +97,15 @@ def statistics(df: pd.DataFrame, col: str, currency: bool):
             x = df[df[col] == el].describe()[['salary_from', 'salary_to', 'salary_avg']]
         s = x.style.format(formatter='{:,.0f}')
         display(s)
+
+def tax(df: pd.DataFrame, col: str):
+  """
+  Создвём метод, который нормирует на НДФЛ для каждой страны
+  Аргументы:
+  df - исходный массив данных
+  col - столбец с з/п, который нужно нормировать
+  """
+  df[col][(df['salary_gross'] == False) & ((df['country_name'] == 'Россия') | (df['country_name'] == 'Беларусь'))] = df[col] / 0.87
+  df[col][(df['salary_gross'] == False) & ((df['country_name'] == 'Казахстан') | (df['country_name'] == 'Кыргызстан'))] = df[col] / 0.9
+  df[col][(df['salary_gross'] == False) & (df['country_name'] == 'Украина')] = df[col] / 0.82
+  df[col][(df['salary_gross'] == False) & (df['country_name'] == 'Грузия')] = df[col] / 0.8
